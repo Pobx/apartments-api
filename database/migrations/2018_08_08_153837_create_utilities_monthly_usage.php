3da9263d081c71a20d36989eb702b679f11ba7ca
Pobx <geidtiphong@gmail.com>
2018-08-08 15:50:26 +0700

    create migration table: utilities_monthly_usage

---
+++ database/migrations/2018_08_08_153837_create_utilities_monthly_usage.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUtilitiesMonthlyUsage extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('utilities_monthly_usage', function (Blueprint $table)
+        {
+            $table->increments('id');
+            $table->foreign('utility_categories_id')->references('id')->on('utility_categories');
+            $table->foreign('room_id')->references('id')->on('rooms');
+            $table->dateTime('utility_memo_date')->comment('วันที่บันทึกจาก User');
+            $table->float('unit_amount', 10, 2)->comment('ปริมาณการใช้งาน หน่วย');
+            $table->enum('status', ['active', 'disabled'])->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('utilities_monthly_usage');
+    }
+}
