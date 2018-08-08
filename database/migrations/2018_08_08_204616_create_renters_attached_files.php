fe69042849a1bb7a189faf64b16b00d7015201c1
Pobx <geidtiphong@gmail.com>
2018-08-08 20:54:22 +0700

    change length column: attached_path

--- database/migrations/2018_08_08_204616_create_renters_attached_files.php
+++ database/migrations/2018_08_08_204616_create_renters_attached_files.php
@@ -19,7 +19,7 @@ class CreateRentersAttachedFiles extends Migration
             $table->integer('renters_id')->unsigned();
             $table->foreign('renters_id')->references('id')->on('renters');
             $table->string('attached_name', 100)->nullable(true)->default(null)->collation('utf8mb4_general_ci')->comment('ชื่อไฟล์');
-            $table->string('attached_path', 100)->nullable(true)->default(null)->collation('utf8mb4_general_ci');
+            $table->string('attached_path', 200)->nullable(true)->default(null)->collation('utf8mb4_general_ci');
             $table->enum('status', ['active', 'disabled', 'rented_room'])->default('active');
             $table->timestamps();
         });
