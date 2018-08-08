5be8c510dfb8c1c557eee3d14b718d43fb7db657
Pobx <geidtiphong@gmail.com>
2018-08-08 20:38:48 +0700

    edit schema table: renter_partners oK

--- database/migrations/2018_08_08_165831_create_renter_partners.php
+++ database/migrations/2018_08_08_165831_create_renter_partners.php
@@ -16,6 +16,8 @@ class CreateRenterPartners extends Migration
         Schema::create('renter_partners', function (Blueprint $table)
         {
             $table->increments('id');
+            $table->integer('renters_id')->unsigned();
+            $table->foreign('renters_id')->references('id')->on('renters');
             $table->string('first_name', 100)->nullable(true)->default(null)->collation('utf8mb4_general_ci')->comment('ชื่อ');
             $table->string('last_name', 100)->nullable(true)->default(null)->collation('utf8mb4_general_ci')->comment('สกุล');
             $table->string('mobile', 100)->nullable(true)->default(null)->collation('utf8mb4_general_ci')->comment('เบอร์มือถือ');
