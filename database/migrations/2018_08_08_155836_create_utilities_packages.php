36e848674bd2bac46fbfdda750641bf0d9922b6e
Pobx <geidtiphong@gmail.com>
2018-08-08 16:00:27 +0700

    create migration table: utilities_packages

---
+++ database/migrations/2018_08_08_155836_create_utilities_packages.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUtilitiesPackages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('utilities_packages', function (Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('name', 100)->nullable(true)->default(null)->collation('utf8mb4_general_ci')->comment('ชื่อ ประเภทห้อง');
+            $table->enum('status', ['active', 'disabled'])->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('utilities_packages');
+    }
+}
