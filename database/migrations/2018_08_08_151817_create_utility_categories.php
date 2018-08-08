5f6d5a4ec120412ba631882010edb2b1129b4365
Pobx <geidtiphong@gmail.com>
2018-08-08 15:32:59 +0700

    create migration table: utility_categories

---
+++ database/migrations/2018_08_08_151817_create_utility_categories.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUtilityCategories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('utility_categories', function (Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('name', 100)->nullable(true)->default(null)->collation('utf8mb4_general_ci')->comment('ชื่อสิ่งอำนวยความสะดวก');
+            $table->float('price_per_unit_cost', 10, 2)->comment('ราคาต้นทุน น้ำ-ไฟ');
+            $table->float('price_per_unit', 10, 2)->comment('ราคาขาย น้ำ-ไฟ');
+            $table->float('unit_min_rate', 10, 2)->comment('หน่วยขั้นต่ำ');
+            $table->float('unit_min_price', 10, 2)->comment('ราคาขายเหมาหน่วยขั้นต่ำ');
+            $table->enum('status', ['active', 'disabled'])->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('utility_categories');
+    }
+}
