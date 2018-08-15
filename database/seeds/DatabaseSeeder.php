5f9cbf9a0d7399163c4702c1828442cd87c62ae6
Pobx <geidtiphong@gmail.com>
2018-08-15 09:47:14 +0700

    ApartmentsTableSeeder passed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([RoomCategoriesTableSeeder::class]);
+        $this->call([
+            RoomCategoriesTableSeeder::class,
+            ApartmentsTableSeeder::class,
+        ]);
     }
 }
