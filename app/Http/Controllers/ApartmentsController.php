74fbf62f75c11939a6acff59045e78db77a6d1e2
Pobx <geidtiphong@gmail.com>
2018-08-15 09:49:10 +0700

    create ApartmentsControllers

---
+++ app/Http/Controllers/ApartmentsController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Apartments;
+use Illuminate\Http\Request;
+
+class ApartmentsController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function create(Request $request)
+    {
+        $this->validate($request, [
+            'name'   => 'required',
+            'status' => 'required',
+        ]);
+
+        $inputs = $request->all();
+        $room_categories = Apartments::create($inputs);
+
+        return response()->json($room_categories->id, 201);
+    }
+
+    public function update(Request $request)
+    {
+        $this->validate($request, [
+            'id'     => 'required',
+            'name'   => 'required',
+            'status' => 'required',
+        ]);
+
+        $inputs = $request->all();
+        Apartments::updateOrCreate(['id' => $inputs['id']], $inputs);
+
+        return response()->json($inputs, 200);
+    }
+
+    //
+}
