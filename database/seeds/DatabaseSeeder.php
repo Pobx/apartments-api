f48d08719568e85aa5a02d28f24cb47533b679f0
Pobx <geidtiphong@gmail.com>
2018-08-15 10:24:21 +0700

    add RentersTableSeeder to DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoomCategoriesTableSeeder::class,
             ApartmentsTableSeeder::class,
+            RentersTableSeeder::class,
         ]);
     }
 }
