cd8e70facb9530fa0da30c91c5ba50fef6ea2e87
Pobx <geidtiphong@gmail.com>
2018-08-15 15:18:11 +0700

    create model: RentersAttachedFiles

---
+++ app/Models/RentersAttachedFiles.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RentersAttachedFiles extends Model
+{
+    protected $table = 'renters_attached_files';
+
+    protected $fillable = [
+        'renters_id',
+        'attached_name',
+        'attached_path',
+        'status',
+    ];
+}
