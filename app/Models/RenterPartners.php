e6c356595079892e9daca0918988ee56e9a35f9c
Pobx <geidtiphong@gmail.com>
2018-08-15 15:58:11 +0700

    create model: RenterPartners

---
+++ app/Models/RenterPartners.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RenterPartners extends Model
+{
+    protected $table = 'renter_partners';
+
+    protected $fillable = [
+        'renters_id',
+        'first_name',
+        'last_name',
+        'mobile',
+    ];
+}
