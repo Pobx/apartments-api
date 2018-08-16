e74205f190a585aa57f3eb41f9aa727cfe6be0e1
Pobx <geidtiphong@gmail.com>
2018-08-16 09:55:23 +0700

    create model: UtilitiesMonthlyUsage

---
+++ app/Models/UtilitiesMonthlyUsage.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UtilitiesMonthlyUsage extends Model
+{
+    protected $table = 'utilities_monthly_usage';
+
+    protected $fillable = [
+        'name',
+        'room_id',
+        'utility_categories_id',
+        'utility_memo_date',
+        'unit_amount',
+        'status',
+    ];
+}
