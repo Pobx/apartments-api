b2f09b2fcf08eaf8c2eeeffd64f363032ab7ece2
Pobx <geidtiphong@gmail.com>
2018-08-16 10:52:54 +0700

    create UtilityCategoriesController

---
+++ app/Http/Controllers/UtilityCategoriesController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UtilityCategories;
+use Illuminate\Http\Request;
+
+class UtilityCategoriesController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    private $validate = [
+        'status'              => 'required|string',
+        'price_per_unit_cost' => 'required|numeric',
+        'price_per_unit'      => 'required|numeric',
+        'unit_min_rate'       => 'required|numeric',
+        'unit_min_price'      => 'required|numeric',
+        'type'                => 'required',
+        'status'              => 'required',
+    ];
+
+    public function create(Request $request)
+    {
+        $this->validate($request, $this->validate);
+
+        $inputs = $request->all();
+        $result = UtilityCategories::create($inputs);
+
+        return response()->json($result->id, 201);
+    }
+
+    public function update(Request $request)
+    {
+        $this->validate($request, $this->validate);
+
+        $inputs = $request->all();
+        UtilityCategories::updateOrCreate(['id' => $inputs['id']], $inputs);
+
+        return response()->json($inputs, 200);
+    }
+
+    //
+}
