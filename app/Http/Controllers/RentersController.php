c378cd9eeb460ee4993ddccf5cc325eb413e4ba8
pobx <geidtiphong@gmail.com>
2018-12-29 14:50:44 +0700

    get image from rest OK

--- app/Http/Controllers/RentersController.php
+++ app/Http/Controllers/RentersController.php
@@ -44,13 +44,15 @@ class RentersController extends Controller
 
         if (!empty($results))
         {
-            $path                  = $_SERVER['DOCUMENT_ROOT'] . '/public/images/' . $results['attached_file_image'];
-            $results['image_path'] = $_SERVER['DOCUMENT_ROOT'] . '/public/default_images/no-image.png';
+            $path       = $_SERVER['DOCUMENT_ROOT'] . '/public/images/' . $results['attached_file_image'];
+            $image_path = '/public/default_images/no-image.png';
 
             if (file_exists($path) && $results['attached_file_image'] != null)
             {
-                $results['image_path'] = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . '/public/images/' . $results['attached_file_image'];
+                $image_path = '/public/images/' . $results['attached_file_image'];
             }
+
+            $results['image_path'] = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . $image_path;
 
         }
 
