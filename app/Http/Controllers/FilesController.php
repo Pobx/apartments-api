8d25ca383c2cd7d6a2b3d8b1791dc2c68c7ef474
pobx <geidtiphong@gmail.com>
2018-12-30 13:02:02 +0700

    create method: upload_file

---
+++ app/Http/Controllers/FilesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class FilesController extends Controller
+{
+    public function getFiles($file, $path)
+    {
+        $dir        = $_SERVER['DOCUMENT_ROOT'] . $path . $file;
+        $file_path = '/public/default_images/no-image.png';
+
+        if (file_exists($dir) && $file != null)
+        {
+            $file_path = '/public/images/' . $file;
+        }
+
+        $results = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . $file_path;
+
+        return $results;
+    }
+}
--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -6,23 +6,29 @@ use Illuminate\Http\Request;
 
 class UploadController extends Controller
 {
-    public function uploadFile($request, $path = '')
+    public function upload_file(Request $request)
     {
-
         $inputs    = $request->all();
-        $path      = ($path == '' ? $_SERVER['DOCUMENT_ROOT'] . '/images' : $path);
-        $link_name = (!isset($inputs['old_file']) || $inputs['old_file'] == null ? null : $inputs['old_file']);
+        $path      = $_SERVER['DOCUMENT_ROOT'] . '/public/files';
+        $link_name = null;
 
-        if ($request->hasFile('new_file'))
+        if ($request->hasFile('file'))
         {
-            $image       = $request->file('new_file');
+            $image       = $request->file('file');
             $origin_name = $image->getClientOriginalName();
             $link_name   = date('YmdHis') . '_' . uniqid() . '_' . $origin_name;
 
             $image->move($path, $link_name);
         }
 
-        return $link_name;
+        $image     = new FilesController;
+        $link_path = $image->getFiles($link_name, '/public/files/');
+
+        return response()->json(
+            [
+                'link_name' => $link_name,
+                'link_path' => $link_path,
+            ], 200);
     }
 
     public function upload_image(Request $request)
