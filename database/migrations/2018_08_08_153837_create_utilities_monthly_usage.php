aa8caaf4843558fc1d7d71b70ec6984e2f2386ac
pobx <geidtiphong@gmail.com>
2019-02-16 11:48:14 +0700

    add columns:

--- database/migrations/2018_08_08_153837_create_utilities_monthly_usage.php
+++ database/migrations/2018_08_08_153837_create_utilities_monthly_usage.php
@@ -7,6 +7,16 @@ use Illuminate\Support\Facades\Schema;
 class CreateUtilitiesMonthlyUsage extends Migration
 {
     /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('utilities_monthly_usage');
+    }
+
+    /**
      * Run the migrations.
      *
      * @return void
@@ -21,22 +31,13 @@ class CreateUtilitiesMonthlyUsage extends Migration
             $table->foreign('utility_categories_id')->references('id')->on('utility_categories');
             $table->foreign('room_id')->references('id')->on('rooms');
             $table->dateTime('utility_memo_date')->nullable(true)->default(null)->comment('วันที่บันทึกจาก User');
-            $table->float('last_unit_amount', 10, 2)->nullable(true)->default(null)->comment('การใช้งานก่อนหน้า หน่วย');
+            $table->float('latest_unit_amount', 10, 2)->nullable(true)->default(null)->comment('การใช้งานก่อนหน้า หน่วย');
+            $table->float('current_unit_amount', 10, 2)->nullable(true)->default(null)->comment('เลขมิเตอร์ปัจจุบัน หน่วย');
             $table->float('unit_amount', 10, 2)->nullable(true)->default(null)->comment('ปริมาณการใช้งาน หน่วย');
             $table->float('price_per_unit', 10, 2)->nullable(true)->default(null)->comment('ราคาต่อหน่วย');
             $table->float('total_price', 10, 2)->nullable(true)->default(null)->comment('ราคารวม');
             $table->enum('status', ['active', 'disabled'])->default('active');
             $table->timestamps();
         });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('utilities_monthly_usage');
     }
 }
