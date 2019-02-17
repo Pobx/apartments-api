65848a1c118237eb309353744074e403d3c6df3f
pobx <geidtiphong@gmail.com>
2019-02-17 11:31:27 +0700

    create image bill done

--- app/Http/Controllers/BillsMonthlyUsageController.php
+++ app/Http/Controllers/BillsMonthlyUsageController.php
@@ -6,39 +6,93 @@ use App\Models\Rooms;
 
 class BillsMonthlyUsageController extends Controller
 {
-    //
+    private $dateController = null;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+    private $filesController = null;
+
+    private $public_path = null;
+
     public function __construct()
     {
-        //
+        $this->public_path     = $_SERVER['DOCUMENT_ROOT'] . '/public/';
+        $this->dateController  = new DatesController;
+        $this->filesController = new FilesController;
     }
 
     public function create_bill($rooms_id, $month)
     {
         $results = $this->find_bill_data_by_rooms_id_and_month($rooms_id, $month);
 
-        $width          = 500;
-        $height         = 400;
-        $margin_left    = 30;
-        $apartment_name = $results['apartments']['name'] ?? null;
+        $width                            = 500;
+        $height                           = 400;
+        $margin_left                      = 30;
+        $margin_left_items_currency_label = 350;
+        $items_margin_top                 = 200;
+
+        $apartment_name        = $results['apartments']['name'] ?? null;
+        $room_name             = $results['name'] ?? null;
+        $apartment_label       = $apartment_name . ' (' . $room_name . ' )';
+        $utility_memo_date_en  = $results['utility_memo_date_en'] ?? date('Y-m-d');
+        $bill_suffix           = date('mY', strtotime($utility_memo_date_en));
+        $date_month_thai_label = $this->dateController->get_month_thai_full_text($utility_memo_date_en);
+        $room_price_item_label = $results['price'] ?? null;
+        $room_price_label      = 'ค่าห้อง';
+        $items_label           = 'รายการ';
+        $items_currency_label  = 'บาท';
+        $unit_amount_label     = 'หน่วย';
+
+        $utilities_monthly_usage = $results['utilities_monthly_usage'] ?? [];
 
         // header('Content-Type: image/png');
+        $font_light_path  = $this->public_path . 'prompt/Prompt-Regular.ttf';
         $images           = ImageCreate($width, $height);
         $background_color = imagecolorallocate($images, 255, 255, 255);
-        // $text_color       = imagecolorallocate($images, 233, 14, 91);
-        $text_color = imagecolorallocate($images, 0, 0, 0);
-        // imagestring($images, 1, 5, 5, $apartment_name, $text_color);
-        // imagettftext($images, 14, 0, $margin_left, 342, $text_color, $font_bold_path, $groups_title);
-        imagettftext($images, 14, 0, $margin_left, 342, $text_color, null, $apartment_name);
-        imagepng($images);
+        $text_color       = imagecolorallocate($images, 0, 0, 0);
+
+        imagettftext($images, 14, 0, $margin_left, 30, $text_color, $font_light_path, $apartment_label);
+        imagettftext($images, 14, 0, $margin_left, 70, $text_color, $font_light_path, $date_month_thai_label);
+
+        // headers
+        imagettftext($images, 14, 0, $margin_left, 130, $text_color, $font_light_path, $items_label);
+        imagettftext($images, 14, 0, $margin_left_items_currency_label, 130, $text_color, $font_light_path, $items_currency_label);
+        imagettftext($images, 14, 0, 150, 130, $text_color, $font_light_path, $unit_amount_label);
+        imagettftext($images, 14, 0, 230, 130, $text_color, $font_light_path, $unit_amount_label);
+
+        // room pirce
+        imagettftext($images, 14, 0, $margin_left, 170, $text_color, $font_light_path, $room_price_label);
+        imagettftext($images, 14, 0, $margin_left_items_currency_label, 170, $text_color, $font_light_path, $room_price_item_label);
+
+        foreach ($utilities_monthly_usage as $key => $value)
+        {
+            imagettftext($images, 14, 0, $margin_left, $items_margin_top, $text_color, $font_light_path, $value['utilities_categories_name']);
+            imagettftext($images, 14, 0, 170, $items_margin_top, $text_color, $font_light_path, (int) $value['latest_unit_amount']);
+            imagettftext($images, 14, 0, 250, $items_margin_top, $text_color, $font_light_path, (int) $value['current_unit_amount']);
+            imagettftext($images, 14, 0, $margin_left_items_currency_label, $items_margin_top, $text_color, $font_light_path, $value['total_price']);
+
+            $items_margin_top += 20;
+
+        }
+
+        // imagepng($images);
+        // imagedestroy($images);
+        $filename  = 'bills_' . $bill_suffix . '_' . date('YmdHis') . '_' . uniqid() . '_' . '.png';
+        $path      = $this->public_path . 'attached_files/' . $filename;
+        $directory = $this->public_path . 'attached_files/';
+        imagepng($images, $path);
+
+        // Clear Memory
         imagedestroy($images);
 
-        // return response()->json($results, 200);
+        $link_url = $this->filesController->getFiles($filename, $directory);
+
+        return response()->json([
+            'results'   => $results,
+            'directory' => $directory,
+            'filename'  => $filename,
+            'path'      => $path,
+            'link_url'  => $link_url,
+
+        ], 200);
     }
 
     public function find_bill_by_rooms_id($rooms_id)
@@ -79,6 +133,8 @@ class BillsMonthlyUsageController extends Controller
         ])->find($rooms_id);
 
         $results['utility_memo_date_th'] = date('d/m/Y', strtotime('+543 years', strtotime($month)));
+        $results['utility_memo_date_en'] = date('Y-m-d', strtotime($month));
+        $results['utility_memo_year']    = date('Y', strtotime('+543 years', strtotime($month)));
 
         return $results;
     }
--- app/Http/Controllers/DatesController.php
+++ app/Http/Controllers/DatesController.php
@@ -4,6 +4,31 @@ namespace App\Http\Controllers;
 
 class DatesController extends Controller
 {
+    private $list_month_thai = [
+        '01' => 'มกราคม',
+        '02' => 'กุมภาพันธ์',
+        '03' => 'มีนาคม',
+        '04' => 'เมษายน',
+        '05' => 'พฤษภาคม',
+        '06' => 'มิถุนายน',
+        '07' => 'กรกฎาคม',
+        '08' => 'สิงหาคม',
+        '09' => 'กันยายน',
+        '10' => 'ตุลาคม',
+        '11' => 'พฤศจิกายน',
+        '12' => 'ธันวาคม',
+    ];
+
+    public function get_month_thai_full_text($date = null)
+    {
+        $date = $date ?? date('Y-m-d');
+
+        $date_explod = explode('-', $date);
+        $results  = $this->list_month_thai[$date_explod[1]] . ' ' . ($date_explod[0] + 543);
+
+        return $results;
+    }
+
     public function setDateEn($data)
     {
         $date    = str_replace('/', '-', $data);
